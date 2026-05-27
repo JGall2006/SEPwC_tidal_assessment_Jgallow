f6b83ed08ee4a430d76f69eab385c945675b1caf
JGall2006 <wml521@york.ac.uk>
2026-05-27 14:31:28 +0100

    lint test passed

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -5,18 +5,20 @@ Decription: This code completes tidal analysis on datasets provided by the
 BODC, """
 
 # import the modules we need
-import pandas as pd
+
+import argparse
 import datetime
 import os
+import matplotlib.dates as mdates
 import numpy as np
-import uptide
+import pandas as pd
 import pytz
 from scipy import stats as sstats
-import matplotlib.dates as mdates
-import argparse
+import uptide
 
 
 def read_tidal_data(filename):
+    """"Function reads tidal guage data, eliminating any illegitimate values"""
     tide_data = pd.read_csv(filename, skiprows=11, header=None, sep=r'\s+')
 #first 11 rows are header information unessisary so skipped
 
@@ -25,9 +27,9 @@ def read_tidal_data(filename):
         format= '%Y/%m/%d %H:%M:%S',)
 
     tide_data = tide_data.drop([0,1], axis=1)#drops, cycle, date,. in
-    tide_data = tide_data.rename(columns= {2:"Time", 3: "Sea Level", 4:"Residual"})
+    tide_data = tide_data.rename(columns= {2:"Time", 3: "Sea Level",
+                                           4:"Residual"})
     tide_data = tide_data.set_index('Date')
-
     data_cols = ["Sea Level", "Residual"]
 
     for col in data_cols:
@@ -37,21 +39,25 @@ def read_tidal_data(filename):
              np.nan)#if value flagged with N or M, it is set nan
 
         tide_data[col] = tide_data[col].str.replace(r"[Tt]","", regex=True)
+        #any values with t are kept these are reliable data points
 
-
-    tide_data['Sea Level'] = pd.to_numeric(tide_data['Sea Level'], errors='coerce')
-    tide_data['Residual'] = pd.to_numeric(tide_data['Residual'], errors='coerce')
+    tide_data['Sea Level'] = pd.to_numeric(tide_data['Sea Level'],
+                                           errors='coerce')
+    tide_data['Residual'] = pd.to_numeric(tide_data['Residual'],
+                                          errors='coerce')
 
     for col in data_cols:
-        tide_data.loc[tide_data[col]<= -99, col] = np.nan #any error codes set null
+        tide_data.loc[tide_data[col]<= -99, col] = np.nan#error codes set null
 
     return tide_data
 
-def extract_single_year_remove_mean(year, data):
-    Start_year = str(year) + "-01-01"
-    End_year = str(year) + "-12-31"
 
-    year_data = data.loc[Start_year:End_year].copy()
+def extract_single_year_remove_mean(year, data):
+    """extracts data from single year, removes the mean showing change in SL"""
+    start_year = str(year) + "-01-01"
+    end_year = str(year) + "-12-31"
+
+    year_data = data.loc[start_year:end_year].copy()
     year_data_mean = year_data['Sea Level'].mean()#calcualtes mean of SL
     year_data['Sea Level'] -= year_data_mean
 
@@ -59,7 +65,8 @@ def extract_single_year_remove_mean(year, data):
 
 
 def extract_section_remove_mean(start, end, data):
-    section = data.loc[start:end].copy()#Gets data specified in the range(section)
+    """Extracts section of data and removes the mean"""
+    section = data.loc[start:end].copy()#Gets data specified in the range
     section_mean = section['Sea Level'].mean()
     section['Sea Level'] -= section_mean
 
@@ -67,10 +74,13 @@ def extract_section_remove_mean(start, end, data):
 
 
 def join_data(data1, data2):
+    """Joins dataframe of from different BODC files"""
 
     return pd.concat([data1, data2]).sort_index()
 
-def sea_level_rise(data): #this is the usual trend with SL
+
+def sea_level_rise(data):
+    """Function calcuates regression and pvalue of SL data"""
 
     sl = data['Sea Level'].dropna()#drops all nan values in column
     x = mdates.date2num(sl.index)
@@ -79,7 +89,9 @@ def sea_level_rise(data): #this is the usual trend with SL
 
     return regression.slope, regression.pvalue
 
-def tidal_analysis(data, constituents, start_datetime): #this is where the m2... amp pha go
+
+def tidal_analysis(data, constituents, start_datetime):
+    """Function analyses data and calcuales the consituents, so M2,S2 ect"""
 
     sl = data.dropna(subset=['Sea Level'])
     tide = uptide.Tides(constituents)
@@ -95,19 +107,23 @@ def tidal_analysis(data, constituents, start_datetime): #this is where the m2...
 
     return amp, pha
 
-def get_longest_contiguous_data(data):#longest unbroken data strech
+
+def get_longest_contiguous_data(data):
+    """Funcation assesses the longest strech of unbroken data"""
 
     grouping = data['Sea Level'].isna().cumsum()#cumsum groups contiguous data
 
-    valid = data.dropna(subset=['Sea Level']) #singles SL data out
+    valid = data.dropna(subset=['Sea Level'])#singles SL data out
     valid_grouping = grouping.loc[valid.index]
-    
+
     longest = valid_grouping.value_counts().idxmax()
 
     return valid[valid_grouping == longest]
 
 
 def main(args_list=None):
+    """Parser allows command line arguments and prints, M2,S2, regression
+    and p-value in terminal"""
 
     parser = argparse.ArgumentParser(
                      prog="UK Tidal analysis",
@@ -146,8 +162,7 @@ def main(args_list=None):
         next_data = read_tidal_data(extra_file)
         combined_data = join_data(combined_data, next_data)
 
-
-#Harmonic analysis 
+#Harmonic analysis
 
     constituents = ['M2','S2']
     start = datetime.datetime(combined_data.index[0].year,1,1, tzinfo = pytz.utc)
@@ -160,37 +175,14 @@ def main(args_list=None):
 
     slope, p_value = sea_level_rise(combined_data)
 
-    location = os.path.basename(os.path.normpath(dirname))
-    analysis = "\n".join([
-        f"location: {location}",
+
+    if args.verbose:
+        print("\n".join([
+        f"location: {os.path.basename(os.path.normpath(dirname))}",
         f"M2 amp: {float(amp[0]):.4f} m phase: {float(pha[0]):.2f} deg",
         f"S2 amp: {float(amp[1]):.4f} m phase: {float(pha[1]):.2f} deg",
         f"Sea Level Rise Slope:{float(slope):.6f}",
-        f"p-value: {float(p_value):.6f}",])
-
-
-    if verbose:
-        print(analysis)
-
+        f"p-value: {float(p_value):.6f}",]))
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
